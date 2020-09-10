43d2c69392f5e16a605da77e1a13947c58067acf
dixroby arone cuellar <alestilodefe@gmail.com>
2020-09-09 19:31:08 -0500

    Feat: Use createContext, useContext, useReducer.
    En esta parte se ase el uso de createContext, useContext, useReducer, donde un producto al hacer click ya se guarda en un estado global, al igual que el patron Redux.

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import "./css/Product.css"
+import { useStateValue } from './StateProvider';
 
-function Product({title,price,image,rating}) {
+function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
     return (
         <div className="product">
             <div className="product__info">
@@ -22,7 +38,7 @@ function Product({title,price,image,rating}) {
             <img 
                 src={image}
             />
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
